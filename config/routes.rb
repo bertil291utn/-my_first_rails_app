4c960addc70fa8c1b3ef579efabf676edda93b3c
bertil291utn <bertilt4@gmail.com>
2020-05-04 15:11:29 -0500

    Updates for Heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'cars#index'
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
